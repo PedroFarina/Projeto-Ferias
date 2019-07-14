540174154d88e3df58595ba4cb2f8f42b537de5b
Pedro Farina <pedrogfarina@gmail.com>
2019-07-14 19:32:57 -0300

    Adicionando propriedades gerais

---
+++ Life Giver/Life Giver/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Life Giver
+//
+//  Created by Pedro Giuliano Farina on 14/07/19.
+//  Copyright © 2019 Pedro Giuliano Farina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
